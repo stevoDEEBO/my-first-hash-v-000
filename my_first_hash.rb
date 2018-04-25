7512c7b2d8557f83652a48e002b8d3c0cc4b62d7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-25 16:29:10 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-{"me" => "Steve Tee", "kiddos" => ["PAXie", "charlie", "brandi"], "milady" => "HOTmama"}  
+{"me" => "Steve Tee", "kiddos" => ["PAXie", "charlie", "brandi"], "milady" => "HOTmama"}
 end
 
 
@@ -20,7 +20,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest[oil paintings]
 end
 
 def adding
@@ -29,14 +29,14 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                 
   # add 2 muskets to the shipping_manifest hash below
-
-
+  shipping_manifest["muskets"] => 2
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun_powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+  puts shipping_manifest
 end
